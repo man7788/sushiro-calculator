fdb3554a6e03c84d9ef5814122fb78cc10dcdaf5
man7788 <jerman1841@gmail.com>
2023-12-18 06:14:56 +0800

    Add totalContext Provider

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,12 +1,18 @@
 import styles from './App.module.css';
+import { useState } from 'react';
 import Header from './Components/Header';
 import Body from './Components/Body';
+import { totalContext } from './Contexts/totalContext';
 
 function App() {
+  const [total, setTotal] = useState(0);
+
   return (
     <div className={styles.App}>
-      <Header />
-      <Body />
+      <Header total={total} />
+      <totalContext.Provider value={{ total, setTotal }}>
+        <Body />
+      </totalContext.Provider>
     </div>
   );
 }
