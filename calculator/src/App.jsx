65934ba02df12a24fa8803eba2fd36c80b3657bc
man7788 <jerman1841@gmail.com>
2023-12-18 04:08:14 +0800

    Add Body to App render

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,10 +1,12 @@
 import styles from './App.module.css';
 import Header from './Components/Header';
+import Body from './Components/Body';
 
 function App() {
   return (
     <div className={styles.App}>
       <Header />
+      <Body />
     </div>
   );
 }
