5e6bfc8efec17f44db75bb636786bb2bc550e379
man7788 <jerman1841@gmail.com>
2023-12-18 06:56:44 +0800

    Add render Gold

--- calculator/src/Components/Body.jsx
+++ calculator/src/Components/Body.jsx
@@ -1,12 +1,14 @@
 import styles from './Body.module.css';
 import Red from './Dishes/Red';
 import Silver from './Dishes/Silver';
+import Gold from './Dishes/Gold';
 
 const Body = () => {
   return (
     <div className={styles.Body}>
       <Red />
       <Silver />
+      <Gold />
     </div>
   );
 };
