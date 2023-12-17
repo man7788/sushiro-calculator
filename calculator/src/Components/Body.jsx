fb2f2200bd6d02f5b3ac7984e4483a3dc1527ef1
man7788 <jerman1841@gmail.com>
2023-12-18 06:53:48 +0800

    Add render Silver

--- calculator/src/Components/Body.jsx
+++ calculator/src/Components/Body.jsx
@@ -1,10 +1,12 @@
 import styles from './Body.module.css';
-import Red from './Red';
+import Red from './Dishes/Red';
+import Silver from './Dishes/Silver';
 
 const Body = () => {
   return (
     <div className={styles.Body}>
       <Red />
+      <Silver />
     </div>
   );
 };
