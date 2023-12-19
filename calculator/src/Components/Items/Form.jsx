d9f025c9650b66190b3f4262df5b58dec409befe
man7788 <jerman1841@gmail.com>
2023-12-20 07:22:30 +0800

    Add From component and style

---
+++ calculator/src/Components/Items/Form.jsx
@@ -0,0 +1,40 @@
+import styles from './Form.module.css';
+import { useState } from 'react';
+
+const Form = ({ extraPrices, setExtraPrices }) => {
+  const [newPrice, setNewPrice] = useState('');
+
+  const onSubmitTask = (e) => {
+    e.preventDefault();
+
+    const newPriceList = [...extraPrices];
+    newPriceList.push(Number(newPrice));
+
+    setExtraPrices(newPriceList);
+    setNewPrice('');
+  };
+
+  const onhandleChange = (e) => {
+    setNewPrice(e.target.value);
+  };
+
+  return (
+    <>
+      <div className={styles.others}>另加項目</div>
+      <div className={styles.othersForm}>
+        <form method="" onSubmit={onSubmitTask}>
+          <label htmlFor="extra-price"> 價錢:</label>
+          <input
+            onChange={onhandleChange}
+            value={newPrice}
+            type="number"
+            id="extra-price"
+          />
+          <button type="submit">加入</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Form;
