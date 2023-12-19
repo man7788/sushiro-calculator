daf81eee2c79eee207e2507adffd80db7f7f2d7d
man7788 <jerman1841@gmail.com>
2023-12-20 07:22:52 +0800

    Add Extra and From to render

--- calculator/src/Components/Body.jsx
+++ calculator/src/Components/Body.jsx
@@ -1,23 +1,20 @@
+import { useState } from 'react';
 import styles from './Body.module.css';
 import Item from './Items/Item';
+import Form from './Items/Form';
+import Extra from './Items/Extra';
 
 const Body = () => {
+  const [extraPrices, setExtraPrices] = useState([]);
+
   return (
     <div className={styles.Body}>
       <Item name={'紅碟'} price={12} />
       <Item name={'銀碟'} price={17} />
       <Item name={'金碟'} price={22} />
       <Item name={'黑碟'} price={27} />
-      <Item price={8} />
-      <Item price={12} />
-      <Item price={17} />
-      <Item price={19} />
-      <Item price={22} />
-      <Item price={24} />
-      <Item price={27} />
-      <Item price={32} />
-      <Item price={39} />
-      <Item price={59} />
+      <Extra extraPrices={extraPrices} />
+      <Form extraPrices={extraPrices} setExtraPrices={setExtraPrices} />
     </div>
   );
 };
