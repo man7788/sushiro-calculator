02d818e577808b5d146f1cf81b2e73cbf93bbb98
man7788 <jerman1841@gmail.com>
2023-12-21 04:59:38 +0800

    Lift title to Body component

--- calculator/src/Components/Items/Form.jsx
+++ calculator/src/Components/Items/Form.jsx
@@ -19,24 +19,21 @@ const Form = ({ extraPrices, setExtraPrices }) => {
   };
 
   return (
-    <>
-      <div className={styles.others}>另加項目</div>
-      <div className={styles.othersForm}>
-        <form method="" onSubmit={onSubmitTask}>
-          <label htmlFor="extra-price"> 價錢:</label>
-          <input
-            onChange={onhandleChange}
-            value={newPrice}
-            type="number"
-            id="extra-price"
-            min="1"
-            max="999"
-            required
-          />
-          <button type="submit">加入</button>
-        </form>
-      </div>
-    </>
+    <div className={styles.othersForm}>
+      <form method="" onSubmit={onSubmitTask}>
+        <label htmlFor="extra-price"> 價錢:</label>
+        <input
+          onChange={onhandleChange}
+          value={newPrice}
+          type="number"
+          id="extra-price"
+          min="1"
+          max="999"
+          required
+        />
+        <button type="submit">加入</button>
+      </form>
+    </div>
   );
 };
 
