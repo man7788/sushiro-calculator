3489761190dd92b1d701582a0198721eceeb054e
man7788 <jerman1841@gmail.com>
2023-12-22 06:28:42 +0800

    Add Body scroll

--- calculator/src/Components/Controls/Select.jsx
+++ calculator/src/Components/Controls/Select.jsx
@@ -1,9 +1,11 @@
 import styles from './Select.module.css';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import Button from './Button';
+import { BodyContext } from '../../Contexts/BodyContext';
 
 const Select = ({ extraPrices, setExtraPrices }) => {
   const [prices, setPrices] = useState([8, 12, 17, 19, 22, 24, 27, 32, 39, 59]);
+  const BodyDom = useContext(BodyContext);
 
   const onHandleClick = (e) => {
     const re = /\d+/;
@@ -14,6 +16,7 @@ const Select = ({ extraPrices, setExtraPrices }) => {
 
     changeClickedPrice(e.target);
     setExtraPrices(newPriceList);
+    BodyDom.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
   };
 
   const changeClickedPrice = (clickedPrice) => {
