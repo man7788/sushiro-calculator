fd3ba9c8b50a751a3ef33ec7332b12f5bbb5c308
man7788 <jerman1841@gmail.com>
2023-12-22 06:28:26 +0800

    Add BodyContext Provider

--- calculator/src/Components/Body.jsx
+++ calculator/src/Components/Body.jsx
@@ -1,25 +1,32 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import styles from './Body.module.css';
 import Item from './Items/Item';
 import Form from './Controls/Form';
 import Extra from './Items/Extra';
 import Select from './Controls/Select';
+import { extraContext } from '../Contexts/extraContext';
+import { BodyContext } from '../Contexts/BodyContext';
 
 const Body = () => {
   const [extraPrices, setExtraPrices] = useState([]);
+  const allExtraItems = useRef({});
+  const BodyDom = useRef(null);
 
   return (
-    <div className={styles.Body}>
-      <Item name={'紅碟'} price={12} />
-      <Item name={'銀碟'} price={17} />
-      <Item name={'金碟'} price={22} />
-      <Item name={'黑碟'} price={27} />
-      <Extra extraPrices={extraPrices} />
-      <div className={styles.others}>加入項目</div>
-      {/* Not allowed duplicated price */}
-      <Select extraPrices={extraPrices} setExtraPrices={setExtraPrices} />
-      <Form extraPrices={extraPrices} setExtraPrices={setExtraPrices} />
-    </div>
+    <BodyContext.Provider value={BodyDom}>
+      <extraContext.Provider value={{ allExtraItems }}>
+        <div className={styles.Body} ref={BodyDom}>
+          <Item name={'紅碟'} price={12} />
+          <Item name={'銀碟'} price={17} />
+          <Item name={'金碟'} price={22} />
+          <Item name={'黑碟'} price={27} />
+          <Extra extraPrices={extraPrices} />
+          <div className={styles.others}>加入項目</div>
+          <Select extraPrices={extraPrices} setExtraPrices={setExtraPrices} />
+          <Form extraPrices={extraPrices} setExtraPrices={setExtraPrices} />
+        </div>
+      </extraContext.Provider>
+    </BodyContext.Provider>
   );
 };
 
