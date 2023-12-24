e160463cdb4be600ceb5dd0ff3eac0945e742a77
man7788 <jerman1841@gmail.com>
2023-12-24 09:01:49 +0800

    Add showDelete state

--- calculator/src/Components/Body.jsx
+++ calculator/src/Components/Body.jsx
@@ -4,11 +4,13 @@ import Item from './Items/Item';
 import Form from './Controls/Form';
 import Extra from './Items/Extra';
 import Select from './Controls/Select';
+import Toolbar from './Controls/Toolbar';
 import { ExtraContext } from '../Contexts/ExtraContext';
 import { BodyContext } from '../Contexts/BodyContext';
 
 const Body = () => {
   const [extraPrices, setExtraPrices] = useState([]);
+  const [showDelete, setShowDelete] = useState(false);
   const allExtraItems = useRef({});
   const BodyDom = useRef(null);
 
@@ -20,10 +22,22 @@ const Body = () => {
           <Item name={'銀碟'} price={17} />
           <Item name={'金碟'} price={22} />
           <Item name={'黑碟'} price={27} />
-          <Extra extraPrices={extraPrices} setExtraPrices={setExtraPrices} />
+          {extraPrices.length > 0 && (
+            <Toolbar
+              extraPrices={extraPrices}
+              setExtraPrices={setExtraPrices}
+              showDelete={showDelete}
+              setShowDelete={setShowDelete}
+            />
+          )}
+          <Extra
+            extraPrices={extraPrices}
+            setExtraPrices={setExtraPrices}
+            showDelete={showDelete}
+          />
           {/* <div className={styles.others}>加入項目</div> */}
           <Select extraPrices={extraPrices} setExtraPrices={setExtraPrices} />
-          <Form extraPrices={extraPrices} setExtraPrices={setExtraPrices} />
+          {/* <Form extraPrices={extraPrices} setExtraPrices={setExtraPrices} /> */}
         </div>
       </ExtraContext.Provider>
     </BodyContext.Provider>
