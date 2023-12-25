73799047bdf14342a68f868bb06bc932ddf2ac7a
man7788 <jerman1841@gmail.com>
2023-12-26 07:01:42 +0800

    Add Toolbar to render

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -2,17 +2,26 @@ import styles from './App.module.css';
 import { useState } from 'react';
 import Header from './Components/Header/Header';
 import Body from './Components/Body';
+import Toolbar from './Components/Controls/Toolbar';
 import { totalContext } from './Contexts/totalContext';
+import { ExtraContext } from './Contexts/ExtraContext';
 
 const App = () => {
   const [subTotal, setSubTotal] = useState(0);
+  const [extraPrices, setExtraPrices] = useState([]);
 
   return (
     <div className={styles.App}>
       <Header subTotal={subTotal} />
-      <totalContext.Provider value={{ subTotal, setSubTotal }}>
-        <Body />
-      </totalContext.Provider>
+      <ExtraContext.Provider value={{ extraPrices, setExtraPrices }}>
+        <totalContext.Provider value={{ subTotal, setSubTotal }}>
+          <Body />
+          <Toolbar
+          // showDelete={showDelete}
+          // setShowDelete={setShowDelete}
+          />
+        </totalContext.Provider>
+      </ExtraContext.Provider>
     </div>
   );
 };
