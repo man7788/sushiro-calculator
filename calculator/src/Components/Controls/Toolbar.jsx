bc3873e903b26b31f1435cd6ab4c79f519ac9ca8
man7788 <jerman1841@gmail.com>
2023-12-26 08:11:31 +0800

    Add show Select and Delete

--- calculator/src/Components/Controls/Toolbar.jsx
+++ calculator/src/Components/Controls/Toolbar.jsx
@@ -1,12 +1,18 @@
 import styles from './Toolbar.module.css';
-import { useState, useContext } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { ExtraContext } from '../../Contexts/ExtraContext';
 import Select from './Select';
 
 const Toolbar = () => {
-  const { extraPrices, setExtraPrices } = useContext(ExtraContext);
+  const { extraPrices, setExtraPrices, showDelete, setShowDelete } =
+    useContext(ExtraContext);
   const [showSelect, setShowSelect] = useState(false);
-  const [showDelete, setShowDelete] = useState(false);
+
+  useEffect(() => {
+    if (extraPrices.length < 1) {
+      setShowDelete(false);
+    }
+  }, [extraPrices]);
 
   const onShowSelect = () => {
     if (!showSelect) {
@@ -17,27 +23,43 @@ const Toolbar = () => {
   };
 
   const onShowDelete = () => {
-    if (!showDelete) {
-      setShowDelete(!showDelete);
-    } else {
-      setShowDelete(!showDelete);
+    if (extraPrices.length > 0) {
+      if (!showDelete) {
+        setShowDelete(!showDelete);
+      } else {
+        setShowDelete(!showDelete);
+      }
     }
   };
 
   return (
-    <div className={styles.Toolbar}>
+    <div className={styles.ToolbarContainer}>
       {showSelect && (
         <Select extraPrices={extraPrices} setExtraPrices={setExtraPrices} />
       )}
-      <button style={showSelect ? clickedSelect : null} onClick={onShowSelect}>
-        加入
-      </button>
-      <button onClick={onShowDelete}>刪除</button>
+      <div className={styles.Toolbar}>
+        <button
+          style={showSelect ? clickedSelect : null}
+          onClick={onShowSelect}>
+          加入
+        </button>
+        <button
+          style={showDelete ? clickedDelete : null}
+          onClick={onShowDelete}>
+          刪除
+        </button>
+      </div>
     </div>
   );
 };
 
 const clickedSelect = {
+  background: '#fff',
+  color: 'rgb(26, 18, 16)',
+  border: '2px solid rgb(167, 8, 25)',
+};
+
+const clickedDelete = {
   background: '#fff',
   color: 'rgb(26, 18, 16)',
   border: '2px solid rgb(167, 8, 25)',
