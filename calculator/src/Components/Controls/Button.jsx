0bdd56c0cfc36ba370d5983933d602fe2037ad69
man7788 <jerman1841@gmail.com>
2024-02-09 02:05:05 +0800

    Add Button render class name

--- calculator/src/Components/Controls/Button.jsx
+++ calculator/src/Components/Controls/Button.jsx
@@ -1,6 +1,10 @@
+import styles from './Button.module.css';
+
 const Button = ({ price, onHandleClick, clicked }) => {
   return (
-    <button style={clicked} onClick={clicked ? null : onHandleClick}>
+    <button
+      className={clicked ? styles.activeButton : styles.defaultButton}
+      onClick={clicked ? null : onHandleClick}>
       {`$${price}`}
     </button>
   );
